7a34ca9b5789bb41a1a45969630a0cbf60b53c1d
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-07-27 14:42:10 +0900

    delete frontend

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders React + Flask Tutorial link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("React + Flask Tutorial");
-  expect(linkElement).toBeInTheDocument();
-});
